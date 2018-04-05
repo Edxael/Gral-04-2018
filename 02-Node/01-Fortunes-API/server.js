2236601b2061ad1a792da3a190d72133b5e915aa
Edxael <zerphyr.r@gmail.com>
2018-04-05 00:10:38 -0600

    add routes to Fortune-API

--- 02-Node/01-Fortunes-API/server.js
+++ 02-Node/01-Fortunes-API/server.js
@@ -1,16 +1,47 @@
+
+// =====[ Loading Dependencies ]==============================================================
 const app = require('express')()
-// const Data = require('./Data/data')  // <--- I have an error here... 
+const bodyParser = require('body-parser')
+const Data = require('./Data/info.json')
 
-app.get('/', (req, res) => {
-    res.json({ message: "Success..." })
-})
+    // ( Middle-Ware )
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
+
+
+
+
+// =====[ Creating Routes ]===================================================================
+
+// ---------------------------------------------------------------( Get All Records )
 app.get('/fortune', (req, res) => {
     console.log("Request for Fortune....")
-    res.send("HEllo")
+    res.json(Data)
+})
+
+// ----------------------------------------------------------------( Get One Random )
+app.get('/fortune/random', (req, res) => {
+    res.json( Data[Math.floor(Math.random() * Data.length )] )
+})
+
+// ----------------------------------------------------------------( Get One by Id )
+app.get('/fortune/:id', (req, res) => {
+    console.log(req.params)
+    res.json( Data.find((x) => { return x.id == req.params.id }) )
+})
+
+// ----------------------------------------------------------------( Add One Record )
+app.post('/fortune', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
 })
 
 
+
+
+
+// =====[ Server Listener ]===================================================================
 app.listen(5000, (err) => {
     if(err) { throw err }
     console.log('Server Up & Running......')
