e425731c0ff55fd1770b5002685664d75bfd1ec7
Edxael <zerphyr.r@gmail.com>
2018-04-09 16:54:25 -0600

    add regex validation for email, phone, date, time

--- 01-React/02-input-validation/src/MyApp/Main.jsx
+++ 01-React/02-input-validation/src/MyApp/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.css'
 
 export default class extends React.Component{
-    state = { email: '', phone: '' }
+    state = { email: '', phone: '', date: '', time: '' }
 
     render(){
 
@@ -11,6 +11,7 @@ export default class extends React.Component{
             console.log("The Email: ", this.state.email)
             console.log("  ")
             // console.log(  !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test( this.state.email ))
+            this.setState({ email: '' })
         }
 
         const ValPhone = () => {
@@ -22,9 +23,25 @@ export default class extends React.Component{
             this.setState({ phone: '' })
         }
 
+        const ValDate = () => {
+            console.log("Validating Date...")
+            console.log("The Date: ", this.state.date)
+            console.log(  /^\d{4}-\d\d-\d\d$/.test( this.state.date ))
+            console.log("  ")
+            this.setState({ date: '' })
+        }
+
+        const ValTime = () => {
+            console.log("Validating Time...")
+            console.log("The Time: ", this.state.time)
+            console.log(  /([01]?[0-9]{1}|2[0-3]{1}):[0-5]{1}[0-9]{1}/.test( this.state.time ))
+            console.log("  ")
+            this.setState({ time: '' })
+        }
+
         return(
             <div>
-                <h1>Form Validation 1.1</h1>
+                <h1>Form Validation 1.3</h1>
                 <hr/>
 
                 <div className="valsec">
@@ -37,6 +54,18 @@ export default class extends React.Component{
                     <input type="text" placeholder="Phone Number..." value={this.state.phone} onChange={ (e) => { this.setState({ phone: e.target.value }) } } />
                     <br/><br/>
                     <button onClick={ ValPhone }>Submit</button>
+                </div>
+
+                <div className="valsec">
+                    <input type="date" placeholder="Date..." value={this.state.date} onChange={ (e) => { this.setState({ date: e.target.value }) } } />
+                    <br/><br/>
+                    <button onClick={ ValDate }>Submit</button>
+                </div>
+
+                <div className="valsec">
+                    <input type="time" placeholder="Date..." value={this.state.time} onChange={ (e) => { this.setState({ time: e.target.value }) } } />
+                    <br/><br/>
+                    <button onClick={ ValTime }>Submit</button>
                 </div>
 
             </div>
