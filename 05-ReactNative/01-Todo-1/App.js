f89253c72061e79bde781e993cabad0bf787322e
Edxael <zerphyr.r@gmail.com>
2018-04-13 01:04:38 -0600

    update todo array

--- 05-ReactNative/01-Todo-1/App.js
+++ 05-ReactNative/01-Todo-1/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+
+  state = { data: ["Clean Home"], text: '' }
+
+  addTodo = () =>{
+    var newTodo = this.state.text;
+    var arr = this.state.data;
+    arr.push(newTodo);
+    this.setState({data: arr, text: ""});
+  }
+
+  renderTodos = () => {
+    return this.state.data.map( (x) => { return (<Text key={x} >{x}</Text>) } )
+  }
+
   render() {
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Hello Edxael</Text>
+
+        <Text>Edxael Todo App 1.1</Text>
+        <TextInput style={styles.myinput} onChange={ (e) => { this.setState({ text: e.target.value }) } } />
+        <Button title="Add To-Do" onPress={this.addTodo} />
+        { this.renderTodos() }
+        
+
       </View>
     );
   }
@@ -16,8 +35,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(0, 247, 255)',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, 
+  myinput: {
+    height: 40,
+    width: 200,
+    borderColor: "black",
+    borderWidth: 1
+  }
 });
---
+++ 05-ReactNative/01-Todo-1/zinfo.md
@@ -0,0 +1,3 @@
+# My notes while developing
+
+Data...
