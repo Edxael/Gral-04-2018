d603deabf6a597645da227dd7a581b1b31adffdb
Edxael <zerphyr.r@gmail.com>
2018-04-19 22:42:23 -0600

    update before branch 1

--- 03-API/02-GQL-Users/schemas/schema1.js
+++ 03-API/02-GQL-Users/schemas/schema1.js
@@ -1,18 +1,39 @@
+// ===[ REQ-DEP ]======================================================
 const graphql = require('graphql')
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql
 const axios =require('axios')
 
 
 
+// ===[ GraphQL Types ]=================================================
+const CompanyType7 = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+})
+
 const UserType1 = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        company: { 
+            type: CompanyType7,
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                            .then((resp) => { return resp.data })
+            }
+        }
     }
 })
 
+
+
+// ===[ RootQueries ]======================================================
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -27,4 +48,6 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+
+// ===[ Exporting ]======================================================
 module.exports = new GraphQLSchema({ query: RootQuery })
--- 03-API/02-GQL-Users/zinfo.md
+++ 03-API/02-GQL-Users/zinfo.md
@@ -3,8 +3,12 @@
 Information on this project.
 
 
+### To run the GraphQL-Server (Command Terminal in Root)
+- $ nodemon server.js
 
-## ---------------------------------------------
+
+
+---------------------------------------------
 ## Intalling the Json server:
 
 ### Terminal command to install server:
@@ -13,7 +17,48 @@ npm install --save json-server
 ### Line addes to Json to start the server: 
 "json:server": "json-server --watch db.json"
 
-### to start the json server: 
+
+## to start the json server: 
 npm run json:server
 
-## ---------------------------------------------
+##Resources
+- http://localhost:3000/users
+- http://localhost:3000/companies
+
+Home
+- http://localhost:3000
+
+
+
+--------------------------------------------------
+# Example of Query when single 'RootQueryType'
+### Branch Single-RootQuery
+
+{
+  user(id: "77"){
+    age
+    firstName
+    company {
+      id
+      name
+      description
+    }
+  }
+}
+
+---[Answer]-------
+{
+  "data": {
+    "user": {
+      "age": 38,
+      "firstName": "Ayumi Hamasaki",
+      "company": {
+        "id": "1",
+        "name": "Avex",
+        "description": "Japan"
+      }
+    }
+  }
+}
+
+--------------------------------------------------
